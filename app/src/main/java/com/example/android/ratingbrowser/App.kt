1b112bc60de9aeb78ba831b285f7a45823fee0db
Andrei Tanana <andrei.tanana@deltatre.it>
2019-07-24 16:16:33 +0300

    kodein init

---
+++ app/src/main/java/com/example/android/ratingbrowser/App.kt
@@ -0,0 +1,12 @@
+package com.example.android.ratingbrowser
+
+import android.app.Application
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+
+class App : Application(), KodeinAware {
+    override val kodein by Kodein.lazy {
+        import(androidXModule(this@App))
+    }
+}
---
+++ app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentListViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.android.ratingbrowser.screens.tournamentslist
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.kodein
+
+class TournamentListViewModel(app: Application) : AndroidViewModel(app), KodeinAware {
+    override val kodein by kodein()
+}
