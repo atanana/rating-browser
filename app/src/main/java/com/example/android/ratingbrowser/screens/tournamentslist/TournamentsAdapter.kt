a6b5bdb5d67237fd55e1bf0c2e2e29ff6a9c8574
Andrei Tanana <andrei.tanana@deltatre.it>
2019-07-31 16:23:24 +0300

    add tournament colors

--- app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentsAdapter.kt
+++ app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentsAdapter.kt
@@ -1,10 +1,16 @@
 package com.example.android.ratingbrowser.screens.tournamentslist
 
+import android.content.Context
+import android.content.res.Resources
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.ratingbrowser.R
 import com.example.android.ratingbrowser.data.TournamentShort
+import com.example.android.ratingbrowser.data.TournamentType
+import com.example.android.ratingbrowser.data.TournamentType.*
 import com.example.android.ratingbrowser.utils.inflate
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_tournament.*
@@ -32,11 +38,25 @@ class TournamentsAdapter(private val clickListener: (TournamentShort) -> Unit) :
     inner class TournamentViewHolder(override val containerView: View) :
         RecyclerView.ViewHolder(containerView),
         LayoutContainer {
+
         fun bind(data: TournamentShort) {
             title.text = data.name
             difficulty.text = data.difficulty?.toString() ?: "-"
             date.text = data.endDate.format(DateTimeFormatter.ISO_DATE)
+            card.setCardBackgroundColor(getTournamentColor(data.type, containerView.context))
             containerView.setOnClickListener { clickListener(data) }
+        }
+
+        @ColorInt
+        private fun getTournamentColor(type: TournamentType, context: Context): Int {
+            val colorRes = when (type) {
+                REAL_SYNCH,
+                SYNCH -> R.color.tournament_synch
+                REAL -> R.color.tournament_real
+                COMMON -> R.color.tournament_common
+                else -> R.color.tournament_default
+            }
+            return ResourcesCompat.getColor(context.resources, colorRes, context.theme)
         }
     }
 }
