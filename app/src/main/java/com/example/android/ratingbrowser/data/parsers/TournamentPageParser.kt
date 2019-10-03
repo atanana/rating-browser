e0d19b06d00075de4cd6693572749f95e4f1e3a7
Andrei Tanana <andrei.tanana@deltatre.it>
2019-10-03 15:34:26 +0300

    parse tournament info init

--- app/src/main/java/com/example/android/ratingbrowser/data/parsers/TournamentPageParser.kt
+++ app/src/main/java/com/example/android/ratingbrowser/data/parsers/TournamentPageParser.kt
@@ -1,7 +1,15 @@
 package com.example.android.ratingbrowser.data.parsers
 
 import com.example.android.ratingbrowser.data.Tournament
+import org.jsoup.Jsoup
 
 class TournamentPageParser {
-    fun parse(page: String): Tournament = Tournament("test", "", "", 0, 0, emptyList(), emptyList(), emptyList())
+    fun parse(page: String): Tournament {
+        val document = Jsoup.parse(page)
+        val name = document.select("h1").first().text()
+        val dates = document.select(".dates .date")
+        val startDate = dates.first().text()
+        val endDate = dates.last().text()
+        return Tournament(name, startDate, endDate, 0, 0, emptyList(), emptyList(), emptyList())
+    }
 }
