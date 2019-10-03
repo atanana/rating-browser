cd479378ce4b11beb9351e6294243a4550032953
Andrei Tanana <andrei.tanana@deltatre.it>
2019-10-03 12:04:58 +0300

    add click listener to binding

--- app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentsAdapter.kt
+++ app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentsAdapter.kt
@@ -1,10 +1,6 @@
 package com.example.android.ratingbrowser.screens.tournamentslist
 
-import android.animation.ObjectAnimator
-import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AccelerateDecelerateInterpolator
-import androidx.core.animation.addListener
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.ratingbrowser.databinding.ItemMonthBinding
@@ -12,7 +8,7 @@ import com.example.android.ratingbrowser.databinding.ItemTournamentBinding
 import com.example.android.ratingbrowser.utils.inflater
 
 class TournamentsAdapter(private val clickListener: (Int) -> Unit) :
-    RecyclerView.Adapter<TournamentsAdapter.TournamentItemViewHolder>() {
+    RecyclerView.Adapter<TournamentItemViewHolder>() {
     var items: List<TournamentListItem> = emptyList()
         set(value) {
             field = value
@@ -22,8 +18,15 @@ class TournamentsAdapter(private val clickListener: (Int) -> Unit) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TournamentItemViewHolder {
         val inflater = parent.inflater()
         return when (viewType) {
-            ITEM_TYPE_TOURNAMENT -> TournamentViewHolder(ItemTournamentBinding.inflate(inflater, parent, false))
-            ITEM_TYPE_MONTH -> MonthSeparatorViewHolder(ItemMonthBinding.inflate(inflater, parent, false))
+            ITEM_TYPE_TOURNAMENT -> {
+                val binding = ItemTournamentBinding.inflate(inflater, parent, false)
+                binding.clickListener = clickListener
+                TournamentViewHolder(binding)
+            }
+            ITEM_TYPE_MONTH -> {
+                val binding = ItemMonthBinding.inflate(inflater, parent, false)
+                MonthSeparatorViewHolder(binding)
+            }
             else -> throw IllegalStateException("Unknown type $viewType")
         }
     }
@@ -43,17 +46,15 @@ class TournamentsAdapter(private val clickListener: (Int) -> Unit) :
             is MonthSeparator -> ITEM_TYPE_MONTH
         }
 
-    abstract class TournamentItemViewHolder(binding: ViewDataBinding) :
-        RecyclerView.ViewHolder(binding.root)
-
-    inner class MonthSeparatorViewHolder(val binding: ItemMonthBinding) :
-        TournamentItemViewHolder(binding)
-
-    inner class TournamentViewHolder(val binding: ItemTournamentBinding) :
-        TournamentItemViewHolder(binding)
-
     companion object {
         private const val ITEM_TYPE_TOURNAMENT = 0
         private const val ITEM_TYPE_MONTH = 1
     }
 }
+
+abstract class TournamentItemViewHolder(binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root)
+
+class MonthSeparatorViewHolder(val binding: ItemMonthBinding) : TournamentItemViewHolder(binding)
+
+class TournamentViewHolder(val binding: ItemTournamentBinding) : TournamentItemViewHolder(binding)
