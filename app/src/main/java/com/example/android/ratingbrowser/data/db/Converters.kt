90649cdb90c6efd49b13a0b09b693ec306c1b3be
Andrei Tanana <andrei.tanana@deltatre.it>
2019-11-14 14:39:01 +0300

    add converters

--- app/src/main/java/com/example/android/ratingbrowser/data/db/Converters.kt
+++ app/src/main/java/com/example/android/ratingbrowser/data/db/Converters.kt
@@ -1,6 +1,8 @@
 package com.example.android.ratingbrowser.data.db
 
 import androidx.room.TypeConverter
+import com.example.android.ratingbrowser.data.TournamentType
+import org.threeten.bp.LocalDate
 
 class Converters {
     @TypeConverter
@@ -8,4 +10,16 @@ class Converters {
 
     @TypeConverter
     fun toPersonRelationType(type: Int): PersonRelationType = PersonRelationType.values()[type]
+
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate): String = date.toString()
+
+    @TypeConverter
+    fun toLocalDate(data: String): LocalDate = LocalDate.parse(data)
+
+    @TypeConverter
+    fun fromTournamentType(type: TournamentType): Int = type.ordinal
+
+    @TypeConverter
+    fun toTournamentType(type: Int): TournamentType = TournamentType.values()[type]
 }
