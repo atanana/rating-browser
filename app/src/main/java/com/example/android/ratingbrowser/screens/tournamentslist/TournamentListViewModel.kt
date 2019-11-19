946b36b4d29bcb3c13c6dbe9bad7b498a8c3a729
Andrei Tanana <andrei.tanana@deltatre.it>
2019-11-19 12:23:21 +0300

    refactoring

--- app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentListViewModel.kt
+++ app/src/main/java/com/example/android/ratingbrowser/screens/tournamentslist/TournamentListViewModel.kt
@@ -3,28 +3,25 @@ package com.example.android.ratingbrowser.screens.tournamentslist
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
 import com.example.android.ratingbrowser.R
 import com.example.android.ratingbrowser.data.StateWrapper
 import com.example.android.ratingbrowser.data.StateWrapper.*
 import com.example.android.ratingbrowser.screens.BaseViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
 import org.kodein.di.generic.instance
 import timber.log.Timber
 
 class TournamentListViewModel(app: Application) : BaseViewModel(app) {
     private val tournamentUsecaseUsecase: TournamentListUsecase by instance()
 
-    val tournaments: LiveData<StateWrapper<TournamentsList>> = liveData {
+    val tournaments: LiveData<StateWrapper<TournamentsList>> = liveData(Dispatchers.IO) {
         emit(Loading())
-        viewModelScope.launch {
-            try {
-                emit(Ok(tournamentUsecaseUsecase.get()))
-            } catch (e: Exception) {
-                Timber.e(e)
-                val errorMessage = app.getString(R.string.error_get_tournaments)
-                emit(Error(errorMessage))
-            }
+        try {
+            emit(Ok(tournamentUsecaseUsecase.get()))
+        } catch (e: Exception) {
+            Timber.e(e)
+            val errorMessage = app.getString(R.string.error_get_tournaments)
+            emit(Error(errorMessage))
         }
     }
 }
